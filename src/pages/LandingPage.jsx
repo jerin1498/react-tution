fc5564aef3d441983502c42466b2978694d85cb4
developer-glareus <developer.glareus@gmail.com>
2022-12-09 19:02:06 +0530

    initial commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/Navbar";
+import LoginFrom from "../components/LoginForm";
+
+function LandingPage() {
+  return (
+    <div>
+      <NavBar />
+      <LoginFrom />
+    </div>
+  );
+}
+
+export default LandingPage;
